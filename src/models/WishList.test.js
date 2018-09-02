a6972eab0e21036143eb4901b414f3d2f79b660d
Hilder Santos <hilder@hildersantos.com>
2018-09-02 16:53:25 -0300

    WishList + tests

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,11 +1,25 @@
-import { WishListItem } from "./WishList";
+import { WishListItem, WishList } from "./WishList";
 
 it("can create an instance of a model", () => {
   const item = WishListItem.create({
     name: "Cronicas de Narnia",
-    price: 39.99,
-    image: "https://placehold.it/400x200"
+    price: 39.99
   });
 
   expect(item.price).toBe(39.99);
+  expect(item.image).toBe("");
+});
+
+it("can create an wishlist", () => {
+  const wishlist = WishList.create({
+    items: [
+      {
+        name: "Cronicas de Narnia",
+        price: 39.99
+      }
+    ]
+  });
+
+  expect(wishlist.items.length).toBe(1);
+  expect(wishlist.items[0].price).toBe(39.99);
 });
