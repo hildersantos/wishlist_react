103587fcfcf1e1785f49094a33196db1abfd4390
Hilder Santos <hilder@hildersantos.com>
2018-09-02 17:11:04 -0300

    WishListItem changeName

--- src/models/WishList.js
+++ src/models/WishList.js
@@ -1,10 +1,17 @@
 import { types } from "mobx-state-tree";
 
-export const WishListItem = types.model({
-  name: types.string,
-  price: types.number,
-  image: ""
-});
+export const WishListItem = types
+  .model({
+    name: types.string,
+    price: types.number,
+    image: ""
+  })
+  .actions(self => {
+    function changeName(newName) {
+      return (self.name = newName);
+    }
+    return { changeName };
+  });
 
 export const WishList = types.model({
   items: types.optional(types.array(WishListItem), [])
--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,23 +1,29 @@
 import { WishListItem, WishList } from "./WishList";
 
+const data = {
+  name: "Cronicas de Narnia",
+  price: 39.99
+};
 it("can create an instance of a model", () => {
-  const item = WishListItem.create({
-    name: "Cronicas de Narnia",
-    price: 39.99
-  });
+  const item = WishListItem.create(data);
 
   expect(item.price).toBe(39.99);
   expect(item.image).toBe("");
 });
 
+it("can change an item name", () => {
+  const item = WishListItem.create(data);
+
+  expect(item.name).toBe("Cronicas de Narnia");
+
+  item.changeName("O Hobbit");
+
+  expect(item.name).toBe("O Hobbit");
+});
+
 it("can create an wishlist", () => {
   const wishlist = WishList.create({
-    items: [
-      {
-        name: "Cronicas de Narnia",
-        price: 39.99
-      }
-    ]
+    items: [data]
   });
 
   expect(wishlist.items.length).toBe(1);
