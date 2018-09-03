e73ffcc8d50a7c73f58efc7f2211da22e648695c
Hilder Santos <hilder@hildersantos.com>
2018-09-03 15:26:52 -0300

    UI and Mobx connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/index.css";
+import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { WishList } from "./models/WishList";
+
+const wishList = WishList.create({
+  items: [
+    {
+      name: "Lego Mindstorms EV3",
+      price: 349.95,
+      image: "https://via.placeholder.com/300x350?text=Lego"
+    },
+    {
+      name: "Miracles - C.S. Lewis",
+      price: 12.91,
+      image: "https://via.placeholder.com/300x350?text=Miracles"
+    }
+  ]
+});
+
+ReactDOM.render(<App wishList={wishList} />, document.getElementById("root"));
