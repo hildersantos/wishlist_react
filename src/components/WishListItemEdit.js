b445c275235811ee142a84941e7ea36a136cc20c
Hilder Santos <hilder@hildersantos.com>
2018-09-03 16:59:57 -0300

    EditForm using MobX State Tree

---
+++ src/components/WishListItemEdit.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { observer } from "mobx-react";
+
+class WishListItemEdit extends Component {
+  render() {
+    const { item } = this.props;
+    return (
+      <div className="item-edit">
+        Thing:{" "}
+        <input type="text" value={item.name} onChange={this.onNameChange} />
+        <br />
+        Price:{" "}
+        <input type="text" value={item.price} onChange={this.onPriceChange} />
+        <br />
+        Image:{" "}
+        <input type="text" value={item.image} onChange={this.onImageChange} />
+        <br />
+      </div>
+    );
+  }
+  onNameChange = event => {
+    this.props.item.changeName(event.currentTarget.value);
+  };
+  onPriceChange = event => {
+    const price = parseInt(event.currentTarget.value);
+    if (!isNaN(price)) this.props.item.changePrice(price);
+  };
+  onImageChange = event => {
+    this.props.item.changeImage(event.currentTarget.value);
+  };
+}
+
+export default observer(WishListItemEdit);
--- src/components/WishListItemView.js
+++ src/components/WishListItemView.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
 import { observer } from "mobx-react";
+import { clone, getSnapshot, applySnapshot } from "mobx-state-tree";
 
-const WishListItemView = ({ item }) => (
-  <li className="item">
-    {item.image && <img src={item.image} />}
-    <h3>{item.name}</h3>
-    <span>{item.price}</span>
-  </li>
-);
+import WishListItemEdit from "./WishListItemEdit";
+
+class WishListItemView extends Component {
+  state = {
+    isEditing: false
+  };
+  render() {
+    const { item } = this.props;
+    return this.state.isEditing ? (
+      this.renderEditable()
+    ) : (
+      <li className="item">
+        {item.image && <img src={item.image} />}
+        <h3>{item.name}</h3>
+        <span>{item.price}</span>
+        <span>
+          <button onClick={this.onToggleEdit}>✎</button>
+        </span>
+      </li>
+    );
+  }
+  renderEditable = () => {
+    return (
+      <li className="item">
+        <WishListItemEdit item={this.state.clone} />
+        <button onClick={this.onSaveEdit}>☑︎</button>
+        <button onClick={this.onCancelEdit}>☒</button>
+      </li>
+    );
+  };
+  onToggleEdit = () => {
+    this.setState({
+      isEditing: true,
+      clone: clone(this.props.item) // clono o item no state
+    });
+  };
+  onCancelEdit = () => {
+    this.setState({
+      isEditing: false
+    });
+  };
+  onSaveEdit = () => {
+    applySnapshot(this.props.item, getSnapshot(this.state.clone)); // aplico as edições do clone no item, através dos snapshots.
+    this.setState({
+      isEditing: false,
+      clone: null
+    });
+  };
+}
 
 export default observer(WishListItemView);
