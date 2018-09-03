86ecf3a32e35070eb735713a3fbe833e4a6889d4
Hilder Santos <hilder@hildersantos.com>
2018-09-03 13:55:18 -0300

    totalPrice + tests

--- src/models/WishList.js
+++ src/models/WishList.js
@@ -26,4 +26,9 @@ export const WishList = types
     addItem(item) {
       self.items.push(item);
     }
+  }))
+  .views(self => ({
+    get totalPrice() {
+      return self.items.reduce((sum, entry) => sum + entry.price, 0);
+    }
   }));
