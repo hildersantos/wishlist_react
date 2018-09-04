d783dbdbe0f72e294d9d34111e90843e76e7227e
Hilder Santos <hilder@hildersantos.com>
2018-09-04 14:49:30 -0300

    Using controller to stop request

--- src/models/Group.js
+++ src/models/Group.js
@@ -25,52 +25,71 @@ export const Group = types
   .model({
     users: types.map(User)
   })
-  .actions(self => ({
-    afterCreate() {
-      self.load();
-    },
-    load: flow(function* load() {
-      const response = yield window.fetch(`http://localhost:3001/users`);
-      const users = yield response.json();
-      applySnapshot(
-        self.users,
-        users.reduce((base, user) => ({ ...base, [user.id]: user }), {})
-      );
-    }),
-    drawLots() {
-      const allUsers = values(self.users);
+  .actions(self => {
+    let controller;
 
-      // se não tiver usuários suficientes...
-      if (allUsers.length <= 1) return;
+    return {
+      afterCreate() {
+        self.load();
+      },
+      beforeDestroy: () => {
+        if (controller) controller.abort();
+      },
+      load: flow(function* load() {
+        controller = window.AbortController && new window.AbortController();
+        try {
+          const response = yield window.fetch(`http://localhost:3001/users`, {
+            signal: controller && controller.signal
+          });
+          const users = yield response.json();
+          applySnapshot(
+            self.users,
+            users.reduce((base, user) => ({ ...base, [user.id]: user }), {})
+          );
+          console.log("success");
+        } catch (e) {
+          console.log("aborted", e.name);
+        }
+      }),
+      reload: () => {
+        if (controller) controller.abort();
+        self.load();
+      },
+      drawLots() {
+        const allUsers = values(self.users);
 
-      let remaining = allUsers.slice();
+        // se não tiver usuários suficientes...
+        if (allUsers.length <= 1) return;
 
-      // allUsers.forEach(user => (user.recipient = undefined));
+        let remaining = allUsers.slice();
 
-      allUsers.forEach(user => {
-        // edge case: the only person without recipient
-        // is the same as the only remaining lot
-        // swap lots with some random other person
-        if (remaining.length === 1 && remaining[0] === user) {
-          const swapWith =
-            allUsers[Math.floor(Math.random() * (allUsers.length - 1))];
-          user.recipient = swapWith.recipient;
-          swapWith.recipient = self;
-          console.log("Swapped!");
-        } else
-          while (!user.recipient) {
-            // Pick random lot of remaining
-            let recipientIndex = Math.floor(Math.random() * remaining.length);
-            let recipient = remaining[recipientIndex];
+        // allUsers.forEach(user => (user.recipient = undefined));
 
-            // If it is not the current user, assign it as recipient
-            // and remove the lot
-            if (recipient !== user) {
-              user.recipient = recipient;
-              console.log(user.name, recipient.name);
-              remaining.splice(recipientIndex, 1);
+        allUsers.forEach(user => {
+          // edge case: the only person without recipient
+          // is the same as the only remaining lot
+          // swap lots with some random other person
+          if (remaining.length === 1 && remaining[0] === user) {
+            const swapWith =
+              allUsers[Math.floor(Math.random() * (allUsers.length - 1))];
+            user.recipient = swapWith.recipient;
+            swapWith.recipient = self;
+            console.log("Swapped!");
+          } else
+            while (!user.recipient) {
+              // Pick random lot of remaining
+              let recipientIndex = Math.floor(Math.random() * remaining.length);
+              let recipient = remaining[recipientIndex];
+
+              // If it is not the current user, assign it as recipient
+              // and remove the lot
+              if (recipient !== user) {
+                user.recipient = recipient;
+                console.log(user.name, recipient.name);
+                remaining.splice(recipientIndex, 1);
+              }
             }
-          }
-      });
-    }
-  }));
+        });
+      }
+    };
+  });
