fef2c2a805299fdecc17c6df3ec475e5bcbb692b
Hilder Santos <hilder@hildersantos.com>
2018-09-03 21:06:26 -0300

    Async/Await tasks

--- src/models/Group.js
+++ src/models/Group.js
@@ -1,12 +1,26 @@
 import { types } from "mobx-state-tree";
 import { WishList } from "./WishList";
 
-const User = types.model({
-  id: types.string,
-  name: types.string,
-  gender: types.enumeration("gender", ["m", "f"]),
-  wishList: types.optional(WishList, {})
-});
+const User = types
+  .model({
+    id: types.string,
+    name: types.string,
+    gender: types.enumeration("gender", ["m", "f"]),
+    wishList: types.optional(WishList, {})
+  })
+  .actions(self => ({
+    async getSuggestions() {
+      const response = await window.fetch(
+        `http://localhost:3001/suggestions_${self.gender}`
+      );
+
+      const suggestions = await response.json();
+      self.addSuggestions(suggestions);
+    },
+    addSuggestions(suggestions) {
+      self.wishList.items.push(...suggestions);
+    }
+  }));
 
 export const Group = types.model({
   users: types.map(User)
